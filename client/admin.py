61043b22f2c53838468b71092953158c3de80605
DovydasAL <davidlazauskas1@gmail.com>
2019-05-16 05:59:30 -0400

    add book and userbook models

--- client/admin.py
+++ client/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Book, UserBook
+
+admin.site.register(Book)
+admin.site.register(UserBook)
--- client/models.py
+++ client/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Book(models.Model):
+    name = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class UserBook(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ("user", "book")
